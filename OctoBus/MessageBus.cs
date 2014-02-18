4ee57b9397dc0fb4c38a50d5807616127939c40c
Sebastian Kent <sebastian.kent@memcf.co.uk>
2014-02-18 14:09:16 +0000

    and so it begins!    again

---
+++ OctoBus/MessageBus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace OctoBus
+{
+    public class MessageBus
+    {
+        private ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _model;
+        private string _exchangeName = "OctoBusExchange";
+        public MessageBus()
+        {
+            _connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _connectionFactory.CreateConnection();
+            _model = _connection.CreateModel();
+
+            _model.ExchangeDeclare(_exchangeName, ExchangeType.Fanout, true);
+            _model.QueueDeclare("OctoBusQueue", true, false, false, null);
+
+            _model.QueueBind("OctoBusQueue", _exchangeName, "");
+            
+        }
+
+        public void Send(string message)
+        {
+            var properties = _model.CreateBasicProperties();
+            properties.SetPersistent(true);
+
+            var data = Encoding.Default.GetBytes(message);
+
+            _model.BasicPublish(_exchangeName, "", properties, data);
+        }
+    }
+}
---
+++ OctoBus/NotWorkingMessageBus.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace OctoBus
+{
+    public class NotWorkingMessageBus
+    {
+        private ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _model;
+
+        private string _exchangeName = "OctoBus";
+        private string _queueName = "OctoBusQueue";
+        public NotWorkingMessageBus()
+        {
+            _connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _connectionFactory.CreateConnection();
+            _model = _connection.CreateModel();
+
+            _model.ExchangeDeclare(_exchangeName, ExchangeType.Fanout, true);
+            _model.QueueDeclare(queue:_queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            _model.QueueBind(queue: _queueName, exchange: _exchangeName, routingKey: "");
+        }
+
+        public void Publish(string message)
+        {
+            var properties = _model.CreateBasicProperties();
+            properties.SetPersistent(true);
+
+            var messageData = Encoding.Default.GetBytes(message);
+            //_model.BasicPublish(_exchangeName, "", properties, messageData);
+            _model.BasicPublish(exchange: _exchangeName, routingKey: "", basicProperties: properties, body: messageData);
+        }
+    }
+}
